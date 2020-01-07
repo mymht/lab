7cb2fe3b5c5a03e9d407a6b19a5b4488e801b939
ganyuhua <1090543866@qq.com>
2020-01-07 20:01:22 +0800

    starting

---
+++ server.c
@@ -0,0 +1,15 @@
+#include<stido.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<ctype.h>
+#include<strings.h>
+#include<string.h>
+#include<sys/stat.h>
+#include<pthread.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+
